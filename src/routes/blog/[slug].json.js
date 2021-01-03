20ebad7d45db7784fcfb9a8dd2f9bc09277892d8
erbridge <f@erbridge.co.uk>
2021-01-03 15:14:22 +0000

    Initial commit tweaked from sapper-template

---
+++ src/routes/blog/[slug].json.js
@@ -0,0 +1,31 @@
+import posts from "./_posts.js";
+
+const lookup = new Map();
+
+for (const post of posts) {
+  lookup.set(post.slug, JSON.stringify(post));
+}
+
+export function get(req, res) {
+  // The `slug` parameter is available because this file is called
+  // `[slug].json.js`.
+  const { slug } = req.params;
+
+  if (lookup.has(slug)) {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+
+    res.end(lookup.get(slug));
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "application/json",
+    });
+
+    res.end(
+      JSON.stringify({
+        message: `Not found`,
+      })
+    );
+  }
+}
