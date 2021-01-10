1b93db8d1a9d2ef829a3b4d8aa749605ba24d544
erbridge <f@erbridge.co.uk>
2021-01-10 14:56:00 +0200

    Add support for citing a crosspost in a post

--- src/md-layouts/default.svelte
+++ src/md-layouts/default.svelte
@@ -5,6 +5,7 @@
   export let date;
   export let title;
 
+  export let crosspost = null;
   export let description =
     "Weeknotes are an opportunity to reflect on the past week, and look forward to the next one. Mine reflect on both my personal, and my professional life, so expect a mixture of software stuff, video games, public sector work, existing as a trans person, tabletop roleplaying, and whatever else claims my attention.";
   export let subtitle = null;
@@ -21,6 +22,11 @@
     color: rgba(var(--text-colour), 0.8);
   }
 
+  header aside {
+    max-width: 60%;
+    margin-left: auto;
+  }
+
   @media (min-width: 700px) {
     article {
       display: flex;
@@ -32,13 +38,17 @@
       word-wrap: break-word;
     }
 
-    hgroup {
+    header > div {
       margin-top: -0.2em;
       border-right: 2px solid rgb(var(--accent-colour));
       padding: 0.2em 1em 3em 0;
       position: sticky;
       top: 0.8em;
       will-change: transform;
+    }
+
+    header aside {
+      max-width: 80%;
     }
 
     section {
@@ -54,13 +64,13 @@
   <meta
     property="og:article:published_time"
     content={new Date(date).toISOString().split('T')[0]} />
+
   <meta property="og:article:author" content="F" />
-  <meta property="og:article:section" content="weeknotes" />
 </Head>
 
 <article>
   <header>
-    <hgroup>
+    <div>
       <h1>{title}</h1>
 
       {#if subtitle}
@@ -70,7 +80,17 @@
       <p>
         <LongDate value={date} />
       </p>
-    </hgroup>
+
+      {#if crosspost && crosspost.from}
+        <aside>
+          <p>
+            This post was originally published on
+            <cite><a
+                href={crosspost.from.href}>{crosspost.from.text}</a></cite>.
+          </p>
+        </aside>
+      {/if}
+    </div>
   </header>
 
   <section>
