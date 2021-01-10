04cff86c3966864a733d030be93cef54509d5e81
erbridge <f@erbridge.co.uk>
2021-01-10 15:46:31 +0200

    Refactor the post fetching logic

--- src/routes/inclusivity/index.json.ts
+++ src/routes/inclusivity/index.json.ts
@@ -1,35 +1,9 @@
-import { walkSync } from "@nodelib/fs.walk";
-import frontMatter from "front-matter";
-import fs from "fs";
 import type { ServerResponse } from "http";
-import path from "path";
 import type { Request } from "polka";
-
-const ROUTES_ROOT_PATH = path.resolve("src", "routes");
-const POST_ROOT_PATH = path.resolve(ROUTES_ROOT_PATH, "inclusivity");
-const ALL_PATH_SEP = new RegExp(path.sep, "g");
-
-const getAllPosts = () =>
-  walkSync(POST_ROOT_PATH)
-    .filter((entry) => entry.name.endsWith(".sveltemd"))
-    .map((entry) => {
-      const file = fs.readFileSync(entry.path, "utf-8");
-      const { attributes } = frontMatter<{
-        title: string;
-        subtitle?: string;
-        date: string;
-      }>(file);
-
-      const slug = path
-        .relative(ROUTES_ROOT_PATH, entry.path)
-        .replace(new RegExp(`${path.extname(entry.path)}$`), "")
-        .replace(ALL_PATH_SEP, "/");
-
-      return { ...attributes, slug };
-    });
+import { getAllPosts } from "../../utils/server/getAllPosts";
 
 export function get(_req: Request, res: ServerResponse) {
-  const posts = getAllPosts();
+  const posts = getAllPosts("inclusivity");
 
   res.writeHead(200, {
     "Content-Type": "application/json",
---
+++ src/utils/server/getAllPosts.ts
@@ -0,0 +1,29 @@
+import { walkSync } from "@nodelib/fs.walk";
+import frontMatter from "front-matter";
+import fs from "fs";
+import path from "path";
+
+const ROUTES_ROOT_PATH = path.resolve("src", "routes");
+const ALL_PATH_SEP = new RegExp(path.sep, "g");
+
+export function getAllPosts(rootPath: string) {
+  return walkSync(path.resolve(ROUTES_ROOT_PATH, rootPath))
+    .filter((entry) => entry.name.endsWith(".sveltemd"))
+    .map((entry) => {
+      const file = fs.readFileSync(entry.path, "utf-8");
+      const { attributes } = frontMatter<{
+        crosspost?: { cite: { href: string; text: string } };
+        date: string;
+        description?: string;
+        title: string;
+        subtitle?: string;
+      }>(file);
+
+      const slug = path
+        .relative(ROUTES_ROOT_PATH, entry.path)
+        .replace(new RegExp(`${path.extname(entry.path)}$`), "")
+        .replace(ALL_PATH_SEP, "/");
+
+      return { ...attributes, slug };
+    });
+}
