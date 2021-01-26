f9c5dfbf6af8317fc789aac89f010eb6b347fd5f
erbridge <f@erbridge.co.uk>
2021-01-26 02:05:45 +0200

    Add "mentally ill" to the about

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,12 +4,14 @@
 
 <Head />
 
-<p>Hi! I’m F :wave:</p>
+<h1>hi</h1>
+
+<p>I’m F :wave:</p>
 
 <p>
-  I’m an anarchist, agender, trans, queer, autistic, disabled, vegan, tattooed,
-  magnetic technologist. I used to make games. Now I work to improve public
-  services.
+  I’m an anarchist, agender, trans, queer, autistic, mentally ill, disabled,
+  vegan, tattooed, magnetic technologist. I used to make games. Now I work to
+  improve public services.
 </p>
 
 <p>This is the home of my ramblings.</p>
