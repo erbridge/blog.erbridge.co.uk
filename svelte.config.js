054e7fb9149be33d885adbe0fc6978353d07d5bf
erbridge <f@erbridge.co.uk>
2022-12-01 01:27:44 +0200

    Use the static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import autoprefixer from "autoprefixer";
 import { mdsvex } from "mdsvex";
 import path from "path";
