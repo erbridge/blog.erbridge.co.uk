cfcc3d9124c0b41bf3d49dfc12de08045dc1f047
erbridge <f@erbridge.co.uk>
2023-06-24 10:59:24 +0100

    Set a trailing slash on base URL for feeds

--- src/lib/server/createFeed.ts
+++ src/lib/server/createFeed.ts
@@ -2,7 +2,7 @@ import { Feed } from "feed";
 import type { Author } from "feed/lib/typings";
 import { getAllPosts } from "./getAllPosts";
 
-const BASE_URL = "https://blog.erbridge.co.uk";
+const BASE_URL = "https://blog.erbridge.co.uk/";
 
 export function createFeed(type: "atom" | "json" | "rss2", slug: string) {
 	const feed = new Feed({
@@ -10,7 +10,7 @@ export function createFeed(type: "atom" | "json" | "rss2", slug: string) {
 		title: "erbridge",
 		language: "en-GB",
 		feedLinks: {
-			[type]: `${BASE_URL}/${slug}`,
+			[type]: `${BASE_URL}${slug}`,
 		},
 		link: BASE_URL,
 		description: "F's blog",
@@ -20,7 +20,7 @@ export function createFeed(type: "atom" | "json" | "rss2", slug: string) {
 	const posts = getAllPosts(".");
 
 	for (const post of posts) {
-		const url = `${BASE_URL}/${post.slug}`;
+		const url = `${BASE_URL}${post.slug}`;
 
 		const authors: Author[] = [
 			{
